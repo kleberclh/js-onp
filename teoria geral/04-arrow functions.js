9728f1b51bce4a9ecf0e2e6c1070ac295bcc2ed8
Kleber Coelho <klebercoelhogt@gmail.com>
2025-03-06 06:26:52 -0300

    arrow function

--- teoria geral/04-arrow functions.js
+++ teoria geral/04-arrow functions.js
@@ -3,4 +3,11 @@ function somar(num1, num2) {
 }
 
 console.log(somar(10, 50));
-''
+
+
+const subtrair = (num1, num2) =>{
+  let resultado = num1 - num2
+  return resultado
+}
+
+console.log(subtrair(50,10))
