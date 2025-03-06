2553c236df1b1ff351e1bab5b7a86ea892be477c
Kleber Coelho <klebercoelhogt@gmail.com>
2025-03-06 06:51:50 -0300

    manipulação de arrays

---
+++ teoria geral/05- manipulação de arrays.js
@@ -0,0 +1,9 @@
+const listaCompras = ["feijão", "arroz", "ovo"]
+
+listaCompras.push("leite") // adiciona item no array na ultima posição
+listaCompras.pop() // exclui o utilmo item do array
+
+listaCompras.unshift("frango") // adiciona o item na primeira posição do array
+listaCompras.shift() // exclui o primeiro item do array
+
+console.log(listaCompras)
