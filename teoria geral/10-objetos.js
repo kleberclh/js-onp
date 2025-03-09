3d7a348a73e6fdf7ef977cf667ae6f627d3c0943
Kleber Coelho <klebercoelhogt@gmail.com>
2025-03-08 22:26:06 -0300

    objetos

---
+++ teoria geral/10-objetos.js
@@ -0,0 +1,14 @@
+const pessoa = [
+  {
+    nome: "Kleber",
+    qualificacao: "Dev",
+    idade: "26",
+  },
+  {
+    nome: "Larissa",
+    qualificacao: "Adm",
+    idade: "26",
+  },
+];
+
+console.log(pessoa)
