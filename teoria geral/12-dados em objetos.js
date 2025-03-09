63c5942453732b8bc9612017e866f0e8edd5f481
Kleber Coelho <klebercoelhogt@gmail.com>
2025-03-09 16:06:33 -0300

    dados em obts

---
+++ teoria geral/12-dados em objetos.js
@@ -0,0 +1,11 @@
+const pessoa = {
+  nome: "Kleber",
+  idade: 26,
+  interesses: ["FULL STACK", "FRONT END"],
+  conjuge: {
+    nome: "Lari",
+    idade: 26,
+  },
+};
+
+console.log(pessoa.idade);
