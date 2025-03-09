bb7229efb5aa2bf0c3142b1684260b8ee9aae98c
Kleber Coelho <klebercoelhogt@gmail.com>
2025-03-08 22:39:13 -0300

    manipulação de obts

---
+++ teoria geral/11-manipulacao de objetos.js
@@ -0,0 +1,14 @@
+const pessoa = 
+  {
+    nome: "Kleber",
+    qualificacao: "Dev",
+    idade: "26",
+  }
+
+;
+
+document.write(pessoa)
+
+pessoa.email = "kleber@dev.com"
+pessoa.nome = "lari"
+console.log(pessoa);
