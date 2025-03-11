a697d258cf21215afe7412c84edfaeb5f1ba45dc
Kleber Coelho <klebercoelhogt@gmail.com>
2025-03-10 21:16:59 -0300

    Create 15-2# manipulação de obj.js

---
+++ teoria geral/15-2# manipulação de obj.js
@@ -0,0 +1,13 @@
+const pessoas = {
+  nome: "Kleber",
+  idade: 26,
+  interesses: ["FULL STACK", "FRONT END"],
+  conjuge: {
+    nome: "Lari",
+    idade: 26,
+  },
+};
+
+const pessoa = new Object();
+
+pessoa.nome = "Kleber";
