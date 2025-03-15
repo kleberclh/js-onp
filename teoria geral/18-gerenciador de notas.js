066fceeb9d5db091bf9249c30cd307baddeb7781
Kleber Coelho <klebercoelhogt@gmail.com>
2025-03-14 23:53:30 -0300

    parte 2

--- teoria geral/18-gerenciador de notas.js
+++ teoria geral/18-gerenciador de notas.js
@@ -9,8 +9,21 @@ const gerenciadorDeNotas = {
   adicionarNota: function (tituloNovaNota, conteudoNovaNota) {
     this.notas.push({ titulo: tituloNovaNota, conteudo: conteudoNovaNota });
   },
+
+  buscarNota: function (titulo) {
+    if (this.notas.find((nota) => nota.titulo === titulo)) {
+      return titulo;
+    } else {
+      return "Nenhum conteudo encontrado";
+    }
+  },
 };
 
 gerenciadorDeNotas.adicionarNota("Estudar 4 vezes no dia", "Modulo 2");
+gerenciadorDeNotas.adicionarNota("comprar novo teclado", "Modulo 2");
 
-console.log(gerenciadorDeNotas.notas);
+gerenciadorDeNotas.buscarNota("comprar novo teclado");
+
+console.log(gerenciadorDeNotas.buscarNota("Estudar 4 vezes no dia"));
+
+// console.log(gerenciadorDeNotas.notas);
