136b84117d7ac6e1e46951b51ab937493e65852b
Kleber Coelho <klebercoelhogt@gmail.com>
2025-03-18 21:07:14 -0300

    #

---
+++ teoria geral/20-for In.js
@@ -0,0 +1,7 @@
+const lista_de_compras = ["Feijão", "Arroz", "Macarrão", "Farrinha"];
+
+console.log(lista_de_compras)
+
+for(let chave in lista_de_compras){
+    console.log(lista_de_compras[chave])
+}
